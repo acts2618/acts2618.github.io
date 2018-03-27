b31e73170e9324ab4709c81c48c066de41b7c57a
Filipe Daineze <33236376+jimmyfilips@users.noreply.github.com>
2018-03-27 18:13:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.nav a[href^="#"]').on('click', function(e) {
+		e.preventDefault();
+		document.getElementById("trigg").checked = false;
+		var id = $(this).attr('href'),
+				targetOffset = $(id).offset().top;
+
+		$('html, body').animate({ 
+			scrollTop: targetOffset - 45
+		}, 500);
+	});
+});
