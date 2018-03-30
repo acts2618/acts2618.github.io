4ad55f9372aeb329729b18f64e905827d1d67531
Filipe Daineze <33236376+jimmyfilips@users.noreply.github.com>
2018-03-29 22:23:41 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
-	$('.nav a[href^="#"]').on('click', function(e) {
+$(document).ready(function () {
+	$('a[href^="#"]').on('click', function (e) {
 		e.preventDefault();
 		document.getElementById("trigg").checked = false;
 		var id = $(this).attr('href'),
 				targetOffset = $(id).offset().top;
 
-		$('html, body').animate({ 
+		$('html, body').animate({
 			scrollTop: targetOffset
 		}, 500);
 	});
+    
+    /* pagina de contato */
+    window.onload = function () {
+        var footer = document.getElementById('footer').offsetHeight,
+            menu = document.getElementById("menu").offsetHeight,
+            econtato = document.getElementById("contato"),
+            altura = "calc(100vh - 12rem - "+menu+"px - "+footer+"px);",
+            padding = "calc(6rem + "+menu+"px);";
+        econtato.style.cssText=
+            "min-height:"+altura+"; padding-top:"+padding;
+    }
 });
